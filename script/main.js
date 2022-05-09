ae88d7b977c80a2e4898727e8b7a622b7a93846d
korney <madkorney@gmail.com>
2022-05-10 01:48:11 +0300

    feat: implement keypress and clicks output to textarea

--- script/main.js
+++ script/main.js
@@ -16,7 +16,7 @@ class ScreenKeyboard {
 
   /** constructor */
   constructor(layout) {
-    // todo - create variables
+    // create properties
     this.buttons = layout.buttons.slice();
     this.modifiers = {
       CapsLock: false,
@@ -29,89 +29,10 @@ class ScreenKeyboard {
     };
     this.value = ''; // current value of textarea
     this.currentLanguage = 'en'; // currenttly displayed keyboard language
-
-    // todo - draw html template
-
-    this.keyFrame = document.getElementById('keyboard');
-    for (let i = 0; i < layout.buttonsNumber; i++) {
-      const newBtn = document.createElement('div');
-      const classes = ['key-btn', `btn-size-x${this.buttons[i].size}`, this.buttons[i].type];
-      newBtn.classList.add(...classes);
-      newBtn.id = this.buttons[i].keycode;
-      newBtn.dataset.keycode = this.buttons[i].keycode;
-      // newBtn.innerHTML = this.buttons[i].captionFixed ?
-      // this.buttons[i].caption['fixed'] : this.buttons[i].caption[this.currentLanguage];
-
-      if (!this.buttons[i].captionFixed) {
-        if (this.modifiers.Shift !== this.modifiers.CapsLock) {
-          newBtn.innerHTML = this.buttons[i].caption[`${this.currentLanguage}Shift`];
-        } else {
-          newBtn.innerHTML = this.buttons[i].caption[this.currentLanguage];
-        }
-      } else {
-        newBtn.innerHTML = this.buttons[i].caption.fixed;
-      }
-
-      this.keyFrame.appendChild(newBtn);
-    }
-    // = butn #id for html  and dataset  data-keycode
-    // <div class="key-btn btn-size-x1 regular" id="test1" data-keycode=""> 1</div>
-    // div.classList.add("foo", "bar", "baz");
-    // const cardBlock = document.createElement("div");
-    //   cardBlock.classList.add("card-block");
-    //   const cardItem = document.createElement("div");
-    //   cardItem.classList.add("card-item");
-    //   const cardFront = document.createElement("div");
-    //   const cardBack = document.createElement("div");
-    //   cardFront.classList.add("card_front");
-    //   cardBack.classList.add("card_back");
-
-    //   gameField.append(cardBlock);
-    //   cardBlock.append(cardItem);
-    //   cardItem.append(cardFront);
-    //   cardItem.append(cardBack);
-
-    //   cardItem.dataset.id = id;
-
-    // todo - draw buttons by layout
-    // todo - add listeners
-
-    /**
-  * get system CAPS/NUM ...  state
-  * @return {obj} system toglers states
-  */
-    function getSystemKeyModifiersState() {
-      // todo - loading anim, 'click for keyboard'
-      // todo - loading prompt - 'press "k" to launch keyboard' - and get
-      // todo - system state and langauge
-      //   const keyState = new KeyboardEvent();
-      //   const getSysKeyModsState = {
-      //     'CapsLock': keyState.getModifierState('CapsLock'),
-      //     'ScrollLock': keyState.getModifierState('ScrollLock'),
-      // "Scroll" fr IE9?
-      //     'NumLock': keyState.getModifierState('NumLock'),
-      //   };
-      return 0; // getSysKeyModsState;
-    }
+    this.buttonsNumber = layout.buttonsNumber;
 
     // const keyToggleModifiers = getSystemKeyModifiersState();
     // console.log(keyToggleModifiers); // return states
-  }
-
-  /**
- * build HTML
- */
-  initialHTMLbuild() {
-    // todo - add main>section>Keyboard wrapper > textasrea, keynboard,
-    // todo - status, modal settings
-
-    const HTML_TEMPLATE = `
-      <main>
-      
-      </main>
-      <script type="module" src="script/main.js"></script>
-      `;
-    document.body.innerHTML = HTML_TEMPLATE;
   }
 
   getButtonByKeycode(code) {
@@ -137,13 +58,161 @@ class ScreenKeyboard {
       }
     }
   }
+
+  updateTextArea(pressedBtn) {
+    let outChar = '';
+    if (this.modifiers.Shift !== this.modifiers.CapsLock) {
+      outChar = pressedBtn.caption[`${this.currentLanguage}Shift`];
+    } else {
+      outChar = pressedBtn.caption[this.currentLanguage];
+    }
+
+    switch (pressedBtn.type) {
+      case 'regular':
+        if (pressedBtn.keycode === 'Tab') {
+          this.textArea.value += '    '; // 4 spaces
+        } else {
+          this.textArea.value += outChar;
+        }
+        break;
+      case 'move':
+        // todo cursor movement
+        this.textArea.value += pressedBtn.caption.fixed; // just print arrws
+        break;
+      case 'mod':
+        // todo mods
+        break;
+      case 'edit':
+        // todo del
+        if (pressedBtn.keycode === 'Enter') {
+          this.textArea.value += '\n';
+        }
+        if (pressedBtn.keycode === 'Backspace') {
+          this.textArea.value = this.textArea.value.substring(0, this.textArea.value.length - 1);
+        }
+        if (pressedBtn.keycode === 'Del') {
+          this.textArea.value += ''; //todo delete!
+        }
+        break;
+      default:
+        break;
+    }
+  }
+
+  handleClick(event) {
+    // handle mouse clicks
+    if (event.target.dataset.keycode) {
+      this.updateTextArea(this.getButtonByKeycode(event.target.dataset.keycode));
+    }
+  }
+
+  handleKeypress(event) {
+    // handle key press
+    this.lastKeyPress.innerHTML = `key: ${event.key}&nbsp;&nbsp;code: ${event.code}`;
+    this.updateTextArea(this.getButtonByKeycode(event.code));
+
+    // disable default? either type symbol to textarea or do mods
+  }
+
+  handleKeyDown(event) {
+    if (this.getButtonByKeycode(event.code)) {
+      event.preventDefault();
+      document.getElementById(event.code).classList.add('pressed');
+      this.handleKeypress(event);
+    }
+  }
+
+  handleKeyUp(event) {
+    if (this.getButtonByKeycode(event.code)) {
+      event.preventDefault();
+      document.getElementById(event.code).classList.remove('pressed');
+    // this.handleKeypress(event);
+    }
+  }
+
+  showKeyboard() {
+    // draw keyboard, init DOM elements, setup listeners
+
+    /**
+    * build HTML body
+    */
+    function HTMLBodyFillFromTemplate() {
+      // todo - add main>section>Keyboard wrapper > textasrea, keynboard,
+
+      const HTML_TEMPLATE = `
+        <main>
+        
+        </main>
+        <script type="module" src="script/main.js"></script>
+        `;
+      document.body.innerHTML = HTML_TEMPLATE;
+    }
+    /**
+    * get system CAPS/NUM ...  state
+    * @return {obj} system toglers states
+    */
+    function getSystemKeyModifiersState() {
+      // todo - loading anim, 'click for keyboard'
+      // todo - loading prompt - 'press "k" to launch keyboard' - and get
+      // todo - system state and langauge
+      //   const keyState = new KeyboardEvent();
+      //   const getSysKeyModsState = {
+      //     'CapsLock': keyState.getModifierState('CapsLock'),
+      //     'ScrollLock': keyState.getModifierState('ScrollLock'),
+      // "Scroll" fr IE9?
+      //     'NumLock': keyState.getModifierState('NumLock'),
+      //   };
+
+      // this.modifiers = {
+      //   CapsLock: false,
+      //   ScrollLock: false,
+      //   Alt: false,
+      //   Ctrl: false,
+      //   Shift: false,
+      //   NumLock: false,
+      //   Meta: false,
+      // };
+      return 0; // getSysKeyModsState;
+    }
+
+    // getSystemKeyModifiersState();
+    // HTMLBodyFillFromTemplate();
+    this.textArea = document.getElementById('kb-textarea');
+    this.lastKeyPress = document.getElementById('status-last-keys');
+    this.keyFrame = document.getElementById('keyboard'); // class property - keyboard DOM parent
+    for (let i = 0; i < this.buttonsNumber; i++) { // add buttons - childrens of keyboard
+      const newBtn = document.createElement('div');
+      const classes = ['key-btn', `btn-size-x${this.buttons[i].size}`, this.buttons[i].type];
+      newBtn.classList.add(...classes);
+      newBtn.id = this.buttons[i].keycode;
+      newBtn.dataset.keycode = this.buttons[i].keycode;
+
+      if (!this.buttons[i].captionFixed) {
+        if (this.modifiers.Shift !== this.modifiers.CapsLock) {
+          newBtn.innerHTML = this.buttons[i].caption[`${this.currentLanguage}Shift`];
+        } else {
+          newBtn.innerHTML = this.buttons[i].caption[this.currentLanguage];
+        }
+      } else {
+        newBtn.innerHTML = this.buttons[i].caption.fixed;
+      }
+
+      this.keyFrame.appendChild(newBtn);
+    }
+
+    this.keyFrame.addEventListener('click', this.handleClick.bind(this));
+    window.addEventListener('keydown', this.handleKeyDown.bind(this));
+    window.addEventListener('keyup', this.handleKeyUp.bind(this));
+  }
 }
 
 // ==========================
 
 const stdKeyboard = new ScreenKeyboard(stdLayout);
 
-// stdKeyboard.initialHTMLbuild();
+stdKeyboard.showKeyboard();
+
+// stdKeyboard.hide();
 
 // =================test
 
@@ -190,7 +259,7 @@ const stdKeyboard = new ScreenKeyboard(stdLayout);
 //   ctrlKey: false,
 //   metaKey: false,
 // }));
-  // console.log('`'.toUpperCase());
+// console.log('`'.toUpperCase());
 // }
 // const btn1 = document.getElementById('test1');
 // btn1.addEventListener('click', testclick);
